9b27623942701c55b8717dd5cb17432302285474
Alejandro G. <imthebest_1997@hotmail.com>
2024-04-05 00:18:56 -0500

    Project init

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,7 +1,20 @@
+import { CheckService } from "../domain/use-cases/checks/check-services";
+import { CronService } from "./cron/cron-service";
+
 export class Server{
 
     public static start(){
         console.log('Server started....');
-    }
+        CronService.createJob(
+            '*/5 * * * * *',
+            ()=>{ 
+                const url = 'https://google.com';
+                new CheckService(
+                    ()=>{ console.log(`Service is ok ${url}`) },
+                    (error)=>{ console.log(`Error: ${error}`) }
+                ).execute(url);
+            }
+        );
+    }       
 
 }
